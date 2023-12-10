d3c0b96f5fc8846c0336008f110af62b34a67dd5
bair-ochirov <pactuc@gmail.com>
2023-12-10 19:17:36 +0800

    fix: change code according to task

--- lesson-01/03.js
+++ lesson-01/03.js
@@ -11,7 +11,8 @@
 // console.log(**number); 
 
 let number = 1;
-console.log(number++);
-console.log(++number);
-console.log(number--);
-console.log(--number)
+console.log();
+console.log();
+console.log();
+console.log(number++ + ', ' + ++number + ', ' + number-- + ',' + --number);
+
