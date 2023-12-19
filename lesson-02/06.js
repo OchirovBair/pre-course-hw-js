b6352829a9d8a72ccdf6cd1d3e90f1dd19e9aee4
bair-ochirov <pactuc@gmail.com>
2023-12-19 11:37:49 +0800

    feat: complete task 06 lesson 02

---
+++ lesson-02/06.js
@@ -0,0 +1,15 @@
+let passportWithAddress = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "LA"
+    }
+};
+
+let passportWithAddress1 = structuredClone(passportWithAddress);
+passportWithAddress1.address.city = 'Bobryisk';
+
+console.log(passportWithAddress.address.city);
+console.log(passportWithAddress1.address.city);
+
